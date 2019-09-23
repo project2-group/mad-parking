a6e2682e08e08d854ff69648b5f084e48fc125ad
Ester-Martinez <martinez.ester@gmail.com>
2019-09-23 16:50:20 +0200

    all models created

--- starter-code/models/Parking.js
+++ starter-code/models/Parking.js
@@ -4,12 +4,18 @@ const Schema = mongoose.Schema;
 const parkingSchema = new Schema(
   {
     id_ayto: Number,
-    comments: [{}]
+    comments: [{type : Schema.Types.ObjectId, ref: 'Comment'}],
+    assessment: Number,
   },
   {
     timestamps: true
   }
 );
+parkingSchema.methods.getAssesment = function(comments) {
+  let output = comments.reduce((ac, cu) => ac + +cu.assessment, 0) / comments.length;
+  let average = +output.toFixed(1);
+  return average;
+};
 
 const Parking = mongoose.model("Parking", parkingSchema);
 module.exports = Parking;
