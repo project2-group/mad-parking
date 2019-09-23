ecd1f4877db49fc3c68ac283a6ea632ff3b490a0
Antonio Llamas <antoniollamasln@gmail.com>
2019-09-23 17:05:38 +0200

    refactoring configs

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,19 +14,12 @@ const MongoStore = require('connect-mongo')(session);
 const flash      = require("connect-flash");
     
 
-mongoose
-  .connect('mongodb://localhost/lab-nodemailer', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
-
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+require('./configs/db.config');
 
 // Middleware Setup
 app.use(logger('dev'));
