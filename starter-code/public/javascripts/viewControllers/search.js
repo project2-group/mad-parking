65b8ec396c1e19bfc2de70295d82904765368789
Lorena Poza Díaz <lorenapozadiaz@MacLPD.local>
2019-09-24 16:40:47 +0200

    Search controller

---
+++ starter-code/public/javascripts/viewControllers/search.js
@@ -0,0 +1,148 @@
+// Declare the center point. Show Madrid center. 
+const centerPoint = {
+  lat: 40.4167,
+  lng: -3.70325
+};
+
+// Get the DOM element and draw the map, zooming to the center point. 
+const mapDOM = document.getElementById("map");
+const map = new google.maps.Map(mapDOM, {
+  zoom: 11,
+  center: centerPoint
+});
+
+function geolocalMap() {
+
+  const infoWindow = new google.maps.InfoWindow();
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Location found.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+        map.setZoom(14);
+      },
+      function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
+}
+
+function autocompleteSearch() {
+  
+  var card = document.getElementById("pac-card");
+  var input = document.getElementById("pac-input");
+
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+  var autocomplete = new google.maps.places.Autocomplete(input);
+
+  // Bind the map's bounds (viewport) property to the autocomplete object,
+  // so that the autocomplete requests use the current map bounds for the
+  // bounds option in the request.
+  autocomplete.bindTo("bounds", map);
+
+  // Set the data fields to return when the user selects a place.
+  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
+
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById("infowindow-content");
+  infowindow.setContent(infowindowContent);
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+  autocomplete.addListener("place_changed", function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17); 
+    }
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = "";
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] &&
+          place.address_components[0].short_name) ||
+          "",
+        (place.address_components[1] &&
+          place.address_components[1].short_name) ||
+          "",
+        (place.address_components[2] &&
+          place.address_components[2].short_name) ||
+          ""
+      ].join(" ");
+    }
+
+    infowindowContent.children["place-icon"].src = place.icon;
+    infowindowContent.children["place-name"].textContent = place.name;
+    infowindowContent.children["place-address"].textContent = address;
+    infowindow.open(map, marker);
+  });
+
+}
+
+window.onload = () => {
+  
+  
+
+
+  // axios.get("/").then(({ data }) => {
+  //   data.forEach(parking => {
+  //     new google.maps.Marker({
+  //       position: {
+  //         lat: parking.location.coordinates[1],
+  //         lng: parking.location.coordinates[0]
+  //       },
+  //       title: parking.name,
+  //       map
+  //     });
+  //   });   
+  // });
+
+  geolocalMap();
+  autocompleteSearch();
+  
+};
+
+
+
+
+
