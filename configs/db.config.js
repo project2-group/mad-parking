c494abfcb4a6453491b4ec5810f0e4b076833640
Ester-Martinez <martinez.ester@gmail.com>
2019-09-25 17:01:06 +0200

    changed database access point

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost/madParking', {useNewUrlParser: true})
+  .connect(process.env.MONGO, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
