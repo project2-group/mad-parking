d16172524c0d6e31f3b72a819b8bd3eadfdd2e55
Ester-Martinez <martinez.ester@gmail.com>
2019-09-26 09:57:15 +0200

    added parking details to database

--- models/Parking.js
+++ models/Parking.js
@@ -1,60 +1,70 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const EMAIL_PATTERN = /^(([^<>()\[\]\.,;:\s@\“]+(\.[^<>()\[\]\.,;:\s@\“]+)*)|(\“.+\“))@(([^<>()[\]\.,;:\s@\“]+\.)+[^<>()[\]\.,;:\s@\“]{2,})$/i;
 
 const parkingSchema = new Schema(
   {
     id_ayto: Number,
     nickName: String,
-    comments: [{type : Schema.Types.ObjectId, ref: 'Comment'}],
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
     assessment: Number,
     location: { type: { type: String }, coordinates: [Number] },
-    contact: {
-      address: {
-        address: String,
-        areaCode: String,
-        town: String
-      },
-      phone: {type: Number},
-      email: {type: String, match: [EMAIL_PATTERN, "this is not a correct email"]},
-      web: {type: String}
-    },
-    accesses: [{
+    address: {
       address: String,
-      location: { type: { type: String }, coordinates: [Number] }
-    }],
-    schedule: String,
-    rates: [{
-      description: String,
-      rate: Number
-    }],
-    parkingType: [{
-      name: String,
-      content: String
-    }],
-    accessType: [{
-      name: String,
-      content: String
-    }],
-    paymentType: [{
-      name: String,
-      content: String
-    }],
-    additionalServices: [{
-      name: String,
-      content: String
-    }],
-    information: [{
-      name: String,
-      content: String
-    }]
+      areaCode: String,
+      town: String
+    },
+    accesses: [
+      {
+        address: String,
+        location: { type: { type: String }, coordinates: [Number] }
+      }
+    ],
+    rates: [
+      {
+        description: String,
+        rate: Number,
+        minutesStayInitation: Number,
+        minutesStayEnd: Number
+      }
+    ],
+    parkingType: [
+      {
+        name: String,
+        content: String
+      }
+    ],
+    accessType: [
+      {
+        name: String,
+        content: String
+      }
+    ],
+    paymentType: [
+      {
+        name: String,
+        content: String
+      }
+    ],
+    additionalServices: [
+      {
+        name: String,
+        content: String
+      }
+    ],
+    information: [
+      {
+        name: String,
+        content: String
+      }
+    ]
   },
   {
     timestamps: true
   }
 );
 parkingSchema.methods.getAssesment = function(comments) {
-  let output = comments.reduce((ac, cu) => ac + +cu.assessment, 0) / comments.length;
+  let output =
+    comments.reduce((ac, cu) => ac + +cu.assessment, 0) / comments.length;
   let average = +output.toFixed(1);
   return average;
 };
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -58,7 +58,6 @@ router.get("/update-parkings", (req, res, next) => {
             ) {
               element.latitude = element.latitude.replace(",", ".");
             }
-            console.log(element.name);
             Parking.create({
               id_ayto: element.id,
               location: {
@@ -83,70 +82,140 @@ router.get("/update-parkingDetails", (req, res, next) => {
   Parking.find()
     .then(allParkings => {
       allParkings.forEach(element => {
-        console.log(element.id_ayto);
         parkingDetailsApi
-          .getDetails(element.id_ayto)
-          .then(details => {
-            //Insert data validations for model
+        .getDetails(element.id_ayto)
+        .then(details => {
+            let data = details.data;
+            let updatedAddress;
+            let updatedAccesses = [];
+            let updatedRates = [];
+            let updatedparkingTypes = [];
+            let updatedAccessType = [];
+            let updatedPaymentType = [];
+            let updatedAdditionalServices = [];
+            let updatedInformation = [];
+            if (data.general) {
+              let newAddress;
+              let newAreaCode;
+              let newTown;
+              if (data.general.address) {
+                newAddress = data.general.address.trim();
+              }
+              if (data.general.areaCode && data.general.areaCode !== "      ") {
+                newAreaCode = data.general.areaCode.trim();
+              }
+              if (data.general.town) {
+                newTown = data.general.town.trim();
+              }
+              updatedAddress = {
+                address: newAddress,
+                areaCode: newAreaCode,
+                town: newTown
+              };
+            }
+            if (data.lstAccess) {
+              data.lstAccess.forEach(element => {
+                if (
+                  element.name === "Vehículo" ||
+                  element.name === "Ascensor para Vehículo"
+                ) {
+                  let address = element.address.trim();
+                  if (element.longitude === "" || element.latitude === "") {
+                    return;
+                  } else if (element.longitude > -2 || element.latitude > 42) {
+                    return;
+                  } else if (
+                    element.latitude.includes(",") ||
+                    element.longitude.includes(",")
+                  ) {
+                    element.latitude = element.latitude
+                      .replace(",", ".")
+                      .trim();
+                    element.longitude = element.longitude
+                      .replace(",", ".")
+                      .trim();
+                  }
+                  let location = {
+                    type: "Point",
+                    coordinates: [element.longitude, element.latitude]
+                  };
+                  updatedAccesses.push({
+                    address: address,
+                    location: location
+                  });
+                } else {
+                  return;
+                }
+              });
+            }
+            if (data.lstRates) {
+              data.lstRates.forEach(element => {
+                let description = element.description;
+                let rates = element.rate;
+                let minutesStayInitation = element.minutesStayInitation;
+                let minutesStayEnd = element.minutesStayEnd;
+                updatedRates.push({
+                  description: description,
+                  rates: rates,
+                  minutesStayInitation: minutesStayInitation,
+                  minutesStayEnd: minutesStayEnd
+                });
+              });
+
+              // console.log(data.lstFeatures)
+              // console.log("---------------------------");
+            }
+            if (data.lstFeatures) {
+              data.lstFeatures.forEach(element => {
+                if (element.nameField === "Tipo plaza") {
+                  let name = element.name;
+                  let content = element.content;
+                  updatedparkingTypes.push({ name: name, content: content });
+                }
+                if (element.nameField === "Tipo acceso") {
+                  let name = element.name;
+                  let content = element.content;
+                  updatedAccessType.push({ name: name, content: content });
+                }
+                if (element.nameField === "Tipo pago") {
+                  let name = element.name;
+                  let content = element.content;
+                  updatedPaymentType.push({ name: name, content: content });
+                }
+                if (element.nameField === "Servicios adicionales") {
+                  let name = element.name;
+                  let content = element.content;
+                  updatedAdditionalServices.push({
+                    name: name,
+                    content: content
+                  });
+                }
+                if (element.nameField === "Información") {
+                  let name = element.name;
+                  let content = element.content;
+                  updatedInformation.push({ name: name, content: content });
+                }
+              });
+            }
             Parking.findOneAndUpdate(
-              { id_ayto: details.id_ayto },
+              { id_ayto: data.id},
               {
                 $set: {
-                  contact: {
-                    address: {
-                      address: String,
-                      areaCode: String,
-                      town: String
-                    },
-                    phone: {type: Number},
-                    email: {type: String, match: [EMAIL_PATTERN, "this is not a correct email"]},
-                    web: {type: String}
-                  },
-                  accesses: [{
-                    address: String,
-                    location: { type: { type: String }, coordinates: [Number] }
-                  }],
-                  schedule: String,
-                  rates: [{
-                    description: String,
-                    rate: Number
-                  }],
-                  parkingType: [{
-                    name: String,
-                    content: String
-                  }],
-                  accessType: [{
-                    name: String,
-                    content: String
-                  }],
-                  paymentType: [{
-                    name: String,
-                    content: String
-                  }],
-                  additionalServices: [{
-                    name: String,
-                    content: String
-                  }],
-                  information: [{
-                    name: String,
-                    content: String
-                  }]
-
-
-
-
-
-
-
-
-
-
+                  address: updatedAddress,
+                  accesses: updatedAccesses,
+                  rates: updatedRates,
+                  parkingType: updatedparkingTypes,
+                  accessType: updatedAccessType,
+                  paymentType: updatedPaymentType,
+                  additionalServices: updatedAdditionalServices,
+                  information: updatedInformation
                 }
               },
               { new: true }
             )
               .then(updatedParking => {
-                return;
+                return
+                
               })
               .catch(err => {
                 console.log(err);
@@ -157,7 +226,10 @@ router.get("/update-parkingDetails", (req, res, next) => {
     })
     .catch(err => {
       console.log(err);
-    });
+    })
+    .then(done => {
+      res.redirect('/')
+    })
 });
 
 module.exports = router;
